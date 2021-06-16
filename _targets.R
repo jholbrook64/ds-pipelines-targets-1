056001aa6e2323b6e3f9dba3899af5738b9c78f3
Jack Holbrook <jholbrook@usgs.gov>
2021-06-16 08:28:50 -0700

    commited new file code.R, updated so that 3 of the 5 targets 	build.
    	current issue: for target figure_1_plot dataframe "eval_data" can't be found, even though
    	it can be seen in debugger
    
    	I assume the same issue for the target
    	model_diagnostics_text.txt

--- _targets.R
+++ _targets.R
@@ -1,12 +1,13 @@
 library(targets)
 source("code.R")
 tar_option_set(packages = c("tidyverse", "stringr", "sbtools", "whisker"))
+tar_option_set(debug = "figure_1_png")
 
 list(
   # Get the data from ScienceBase
   tar_target(
     model_RMSEs_csv,
-    download_data(out_filepath = "1_fetch/out/model_RMSEs.csv"),
+    download_data(out_filepath = "1_fetch/out/"),   # removed file name, see if this works in next tar_make() 4:11pm
     format = "file"
   ), 
   # Prepare the data for plotting
@@ -17,19 +18,19 @@ list(
   # Create a plot
   tar_target(
     figure_1_png,
-    make_plot(out_filepath = "3_Visualize/out/figure_1.png", data = eval_data), 
+    make_plot(out_filepath = "3_Visualize/out/", data = eval_data), 
     format = "file"
   ),
   # Save the processed data
   tar_target(
     model_summary_results_csv,
-    write_csv(eval_data, file = "2_process/out/model_summary_results.csv"), 
+    write_csv(eval_data, file = "2_process/out/"), 
     format = "file"
   ),
   # Save the model diagnostics
   tar_target(
     model_diagnostic_text_txt,
-    generate_model_diagnostics(out_filepath = "2_process/out/model_diagnostic_text.txt", data = eval_data), 
+    generate_model_diagnostics(out_filepath = "2_process/out/", data = eval_data), 
     format = "file"
   )
 )
---
+++ code.R
@@ -0,0 +1,151 @@
+## ---------------------------
+##
+## Script name: Code.R
+##
+## Purpose of script: test out the Targets package!
+##
+## Author: Jack Holbrook (USGS)
+##
+## ---------------------------
+## Notes:
+##     ~
+## ---------------------------
+
+   
+download_data <- function(out_filepath)
+{ # Get the data from Science Base 
+  mendota_file <- file.path(out_filepath, 'model_RMSE.csv')
+  item_file_download('5d925066e4b0c4f70d0d0599', names = 'me_RMSE.csv',
+                     destinations = mendota_file, overwrite_file = TRUE)
+  return(mendota_file)
+}
+
+
+process_data <- function(in_filepath)
+{ # Prepare the data for plotting
+  eval_data <- readr::read_csv(in_filepath, col_types = 'iccd') %>%
+    filter(str_detect(exper_id, 'similar_[0-9]+')) %>%
+    mutate(col = case_when(
+      model_type == 'pb' ~ '#1b9e77',
+      model_type == 'dl' ~'#d95f02',
+      model_type == 'pgdl' ~ '#7570b3'
+    ), pch = case_when(
+      model_type == 'pb' ~ 21,
+      model_type == 'dl' ~ 22,
+      model_type == 'pgdl' ~ 23
+    ), n_prof = as.numeric(str_extract(exper_id, '[0-9]+')))
+  return(eval_data)
+}
+
+
+make_plot <- function(out_filepath, data)
+{ 
+  fp <- file.path(out_filepath, 'figure_1.png')
+  png(file = fp, width = 8, height = 10, res = 200, units = 'in')
+  par(omi = c(0,0,0.05,0.05), mai = c(1,1,0,0), las = 1, mgp = c(2,.5,0), cex = 1.5)
+  # I would like to assining this to a vriable but 
+  plot(NA, NA, xlim = c(2, 1000), ylim = c(4.7, 0.75),
+       ylab = "Test RMSE (°C)", xlab = "Training temperature profiles (#)", log = 'x', axes = FALSE)
+  # log x axis vector
+  n_profs <- c(2, 10, 50, 100, 500, 980)
+  # set axis, this cannot be a function becuase 
+  axis(1, at = c(-100, n_profs, 1e10), labels = c("", n_profs, ""), tck = -0.01)
+  axis(2, at = seq(0,10), las = 1, tck = -0.01)
+  # slight horizontal offsets so the markers don't overlap:
+  offsets <- data.frame(pgdl = c(0.15, 0.5, 3, 7, 20, 30)) %>%
+    mutate(dl = -pgdl, pb = 0, n_prof = n_profs)
+  
+  # loop: do this for each type of model in the analysis
+  for (mod in c('pb','dl','pgdl')){
+    #outer
+    # use function parameter here:
+    mod_data <- filter(eval_data, model_type == mod) 
+    mod_profiles <- unique(mod_data$n_prof) 
+    #inner
+    for (mod_profile in mod_profiles) 
+    {
+      d <- filter(mod_data, n_prof == mod_profile) %>% 
+        summarize(y0 = min(rmse), y1 = max(rmse), col = unique(col))
+      x_pos <- offsets %>% filter(n_prof == mod_profile) %>% pull(!!mod) + mod_profile
+      # this is its own thing this line adds the values to a line for the plot
+      lines(c(x_pos, x_pos), c(d$y0, d$y1), col = d$col, lwd = 2.5)
+    }
+    #outer
+    d <- group_by(mod_data, n_prof) %>% 
+      summarize(y = mean(rmse), col = unique(col), pch = unique(pch)) %>%
+      rename(x = n_prof) %>% arrange(x)
+    # adds lines
+    lines(d$x + tail(offsets[[mod]], nrow(d)), d$y, col = d$col[1], lty = 'dashed')
+    # adds points
+    points(d$x + tail(offsets[[mod]], nrow(d)), d$y, pch = d$pch[1], col = d$col[1], bg = 'white', lwd = 2.5, cex = 1.5)
+  }
+  points(2.2, 0.79, col = '#7570b3', pch = 23, bg = 'white', lwd = 2.5, cex = 1.5)
+  text(2.3, 0.8, 'Process-Guided Deep Learning', pos = 4, cex = 1.1)
+  points(2.2, 0.94, col = '#d95f02', pch = 22, bg = 'white', lwd = 2.5, cex = 1.5)
+  text(2.3, 0.95, 'Deep Learning', pos = 4, cex = 1.1)
+  points(2.2, 1.09, col = '#1b9e77', pch = 21, bg = 'white', lwd = 2.5, cex = 1.5)
+  text(2.3, 1.1, 'Process-Based', pos = 4, cex = 1.1)
+  #writes it to the png file
+  dev.off()
+  return(fp)
+}
+
+
+write_csv <-  function(eval_data, file){
+# Save the processed data to '2_process/out'
+readr::write_csv(eval_data, "2_process/out/model_summary_results.csv")    #= file.path(file, 'model_summary_results.csv')
+  return("model_summary_results.csv")
+}
+
+generate_model_diagnostics <-  function(out_filepath, eval_data){  
+  # Save the model diagnostics, write to the processes folder 
+  render_data <- list(pgdl_980mean = filter(eval_data, model_type == 'pgdl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
+                      dl_980mean = filter(eval_data, model_type == 'dl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_980mean = filter(eval_data, model_type == 'pb', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
+                      dl_500mean = filter(eval_data, model_type == 'dl', exper_id == "similar_500") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_500mean = filter(eval_data, model_type == 'pb', exper_id == "similar_500") %>% pull(rmse) %>% mean %>% round(2),
+                      dl_100mean = filter(eval_data, model_type == 'dl', exper_id == "similar_100") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_100mean = filter(eval_data, model_type == 'pb', exper_id == "similar_100") %>% pull(rmse) %>% mean %>% round(2),
+                      pgdl_2mean = filter(eval_data, model_type == 'pgdl', exper_id == "similar_2") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_2mean = filter(eval_data, model_type == 'pb', exper_id == "similar_2") %>% pull(rmse) %>% mean %>% round(2))
+  
+  template_1 <- 'resulted in mean RMSEs (means calculated as average of RMSEs from the five dataset iterations) of {{pgdl_980mean}}, {{dl_980mean}}, and {{pb_980mean}}°C for the PGDL, DL, and PB models, respectively.
+  The relative performance of DL vs PB depended on the amount of training data. The accuracy of Lake Mendota temperature predictions from the DL was better than PB when trained on 500 profiles 
+  ({{dl_500mean}} and {{pb_500mean}}°C, respectively) or more, but worse than PB when training was reduced to 100 profiles ({{dl_100mean}} and {{pb_100mean}}°C respectively) or fewer.
+  The PGDL prediction accuracy was more robust compared to PB when only two profiles were provided for training ({{pgdl_2mean}} and {{pb_2mean}}°C, respectively). '
+  # since this is inside the r function, this should be able to use the local variable render_data
+  whisker.render(template_1 %>% str_remove_all('\n') %>% str_replace_all('  ', ' '), render_data) %>% cat(file = file.path(out_filepath, 'model_diagnostic_text.txt'))
+}
+
+
+########
+# DIRECTIONS :
+# run this code block after adding each function to the functions in your global environment:
+
+# create new output directory
+
+# visualize_output_dir <- '3_Visualize/out'   
+# process_output_dir <- '2_process/out'
+
+# create directories from associated paths: these are kept because functions will throw errors if these don't exist 
+# dir.create(visualize_output_dir)
+
+# get data from the web:
+# Include a parameter for the path to the output folder from the project directory:
+# out_folderFetch <-  '1_fetch/out/'
+# file_out <- getCSV(out_folderFetch)
+
+#create data frame for evaluated data:
+
+# evalData <- eval_data(file_out)
+
+# writes to '3_Visualize/out'  
+# will get a console output of "null device", means that dev.off retyurns the r device to state 1. 
+# createPlot(evalData, plotOutput = visualize_output_dir)   
+
+# writes to '2_process/out'  
+# dataRender(evalData, textOutput = process_output_dir)
+
+
+
+#~######~#
